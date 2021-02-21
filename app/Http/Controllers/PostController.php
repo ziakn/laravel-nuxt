a41c905605498f99b0699424856fb06949a096e7
ziakn <ziakn03@gmail.com>
2021-02-21 14:39:05 +0300

    passport data

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,85 +1,103 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use App\Models\Post;
 use Illuminate\Http\Request;
+use App\Models\Post;
+use Auth;
 
 class PostController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        //
+        // $posts = auth()->user();
+            $data =Post::get();
+ 
+        return response()->json([
+            'success' => true,
+            'data' => $data
+        ]);
     }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+ 
+    public function show($id)
     {
-        //
+        $post = auth()->user()->posts()->find($id);
+ 
+        if (!$post) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Post not found '
+            ], 400);
+        }
+ 
+        return response()->json([
+            'success' => true,
+            'data' => $post->toArray()
+        ], 400);
     }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+ 
     public function store(Request $request)
     {
-        //
-    }
+        $auth_id = Auth::id();
+        // return $auth_id;
+        $this->validate($request, [
+            'title' => 'required',
+            'description' => 'required'
+        ]);
+ 
+        $create = Post::create([
+        'user_id'=>$auth_id,
+        'title'=>$request->title,
+        'description'=>$request->description,
+        ]);
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Post $post)
-    {
-        //
-    }
+        return response()->json($create);
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Post $post)
-    {
-        //
     }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Post $post)
+ 
+    public function update(Request $request, $id)
     {
-        //
+        $post = auth()->user()->posts()->find($id);
+ 
+        if (!$post) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Post not found'
+            ], 400);
+        }
+ 
+        $updated = $post->fill($request->all())->save();
+ 
+        if ($updated)
+            return response()->json([
+                'success' => true
+            ]);
+        else
+            return response()->json([
+                'success' => false,
+                'message' => 'Post can not be updated'
+            ], 500);
     }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Post $post)
+ 
+    public function destroy($id)
     {
-        //
+        $post = auth()->user()->posts()->find($id);
+ 
+        if (!$post) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Post not found'
+            ], 400);
+        }
+ 
+        if ($post->delete()) {
+            return response()->json([
+                'success' => true
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Post can not be deleted'
+            ], 500);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+
+Route::post('register', [PassportAuthController::class, 'register']);
+Route::post('login', [PassportAuthController::class, 'login']);
+
+Route::middleware('auth:api')->group(function () {
+    Route::resource('users', PassportAuthController::class);
+    Route::resource('posts', PostController::class);
+});
