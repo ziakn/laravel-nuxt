c078301533224ae09fab80b0eae5f07a25cfe956
ziakn <ziakn03@gmail.com>
2021-02-21 15:02:19 +0300

    passport and nuxt authetication

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PassportAuthController;
+use App\Http\Controllers\PostController;
+
 
 /*
 |--------------------------------------------------------------------------
