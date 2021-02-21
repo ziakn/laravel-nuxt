da059414fcb13886ac3130eedb6b2c47e45853dc
ziakn <ziakn03@gmail.com>
2021-02-21 16:48:25 +0300

    Images

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,69 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+<!DOCTYPE >
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
-                </div>
-            </div>
-        </div>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <style>
+        .no-js #loader {
+            display: none;
+        }
+
+        .js #loader {
+            display: block;
+            position: absolute;
+            left: 100px;
+            top: 0;
+        }
+
+        #se-pre-con {
+            position: fixed;
+            left: 0px;
+            top: 0px;
+            width: 100%;
+            height: 100%;
+            z-index: 9999;
+            background: url(/Preloader_2.gif) center no-repeat #fff;
+        }
+    </style>
+    <script>
+        (function () {
+            window.Laravel = {
+                csrfToken: '{{ csrf_token() }}'
+            };
+            @if(Auth::check())
+                window.authUser={!! Auth::user() !!}
+                 window.authUserRole={!! Auth::user()->userrole !!}
+                @else
+                window.authUser=false
+                @endif
+        })();
+    </script>
+</head>
+
+<body>
+
+    <div id="se-pre-con"></div>
+
+    <div id="app">
+        <z-dashboard></z-dashboard>
     </div>
-</x-app-layout>
+    <script>
+        window.addEventListener('load', function() {
+                    var s = document.getElementById('se-pre-con').style;
+                    s.opacity = 1;
+                    (function fade(){(s.opacity-=.1)<0?s.display="none":setTimeout(fade,40)})();
+                        // $(".se-pre-con").fadeOut("slow");
+                    })
+    </script>
+    {{-- <script src="{{ asset('firebase-messaging-sw.js') }}"></script> --}}
+    <script src="{{ asset('js/manifest.js') }}"></script>
+    <script src="{{ asset('js/vendor.js') }}"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
+    {{-- <script src="http://localhost:9000/js/app.js"></script> --}}
+</body>
+
+</html>
